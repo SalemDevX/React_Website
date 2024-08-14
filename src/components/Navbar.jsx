fea988bee776b3a067ee30246569b8caafc81cf0
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-08-14 15:38:45 +0100

    Created Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,20 +11,20 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-1 text-white">
       <h1 className=" w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav}>
+      <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden"
             : "fixed left-[-100%]"
         }
       >
