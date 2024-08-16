491f7a850371fbe58898bee0c776cd4a9d43deb1
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-08-16 15:45:15 +0100

    Newsletter component that renders a newsletter subscription form.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Analytics from "./components/Analytics"
+import Newsletter from "./components/Newsletter"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   )
 }
