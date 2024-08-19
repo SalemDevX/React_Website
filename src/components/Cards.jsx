d485125035838718c06f046c29693571b3b963b6
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-08-19 06:33:51 +0100

    Card Component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Single from '../assets/single.png'
+import Double from '../assets/double.png'
+import Triple from '../assets/triple.png'
+
+const Cards = () => {
+  return (
+    <div className='w-full py-[10rem] px-4 bg-white'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+            <div className='w-full '>
+                <img src={Single} alt="/" />
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Cards
