e0339797edd53c94f3faff396eb74cfa69e15996
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-08-20 15:29:18 +0100

    Quick edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,26 +19,24 @@ const Navbar = () => {
         <li className="p-4">Contact</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div
+      <ul
         className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden"
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
             : "fixed left-[-100%]"
         }
       >
         <h1 className=" w-full text-3xl font-bold text-[#00df9a] m-4">
           REACT.
         </h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4">Contact</li>
-        </ul>
-      </div>
+        <li className="p-4 border-b border-gray-600">Home</li>
+        <li className="p-4 border-b border-gray-600">Company</li>
+        <li className="p-4 border-b border-gray-600">Resources</li>
+        <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
     </div>
   );
 };
